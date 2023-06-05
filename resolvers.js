7079c6c3fcf4f1afcc8143aab632a29435ad2013
Ruan Eloy <ruanr18@gmail.com>
2023-06-05 15:23:29 -0300

    get job by id

--- resolvers.js
+++ resolvers.js
@@ -1,9 +1,10 @@
 import { getCompany } from "./db/companies.js";
-import { getJobs } from "./db/jobs.js";
+import { getJob, getJobs } from "./db/jobs.js";
 
 export const resolvers = {
   Query: {
-    jobs: () => getJobs()
+    jobs: () => getJobs(),
+    job: (_root, { id }) => getJob(id),
   },
 
   Job: {
