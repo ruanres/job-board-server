443dc9603de00df3b554480bffb9bfc0877dbd62
Ruan Eloy <ruanr18@gmail.com>
2023-06-05 12:54:41 -0300

    add resolvers and schema

---
+++ resolvers.js
@@ -0,0 +1,15 @@
+import { getJobs } from "./db/jobs.js";
+
+export const resolvers = {
+  Query: {
+    jobs: () => getJobs(),
+  },
+
+  Job: {
+    date: (job) => toIsoDate(job.createdAt),
+  }
+}
+
+function toIsoDate(value) {
+  return value.slice(0, 'yyyy-mm-dd'.length);
+}
