0ea32083512e8c318416264ac04ffc50c4755e63
Ruan Eloy <ruanr18@gmail.com>
2023-06-14 12:00:41 -0300

    add create job mutation

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,6 @@
 import { GraphQLError } from "graphql";
 import { getCompany } from "./db/companies.js";
-import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
+import { createJob, getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
 
 export const resolvers = {
   Query: {
@@ -19,6 +19,13 @@ export const resolvers = {
       }
       return company;
     },
+  },
+
+  Mutation: {
+    createJob: (_root, {input: {title, description}}) => {
+      const companyId = 'FjcJCHJALA4i';
+      return createJob({title, description, companyId});
+    }
   },
 
   Job: {
